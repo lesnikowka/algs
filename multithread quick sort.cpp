51ae74c8cc144d22a6510bc51a9a00f2891011a4
lesnikowka <92714965+lesnikowka@users.noreply.github.com>
2023-09-07 23:49:06 +0300

    Update multithread quick sort.cpp

--- multithread quick sort.cpp
+++ multithread quick sort.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <thread>
 #include <stack>
@@ -6,10 +6,13 @@
 #include <random>
 #include <initializer_list>
 #include <condition_variable>
+#include <ctime>
 
 std::mutex mtx;
 
 std::condition_variable cond;
+
+const size_t NUM_THREADS = std::thread::hardware_concurrency();
 
 
 template<class T>
@@ -55,6 +58,12 @@ void quick_sort(typename T::iterator first, typename T::iterator last, std::stac
 		}
 	}
 
+	if (last - first < 10000) {
+		quick_sort<std::vector<int>>(first, first + medium, tasks);
+		quick_sort<std::vector<int>>(first + right_medium + 1, last, tasks);
+		return;
+	}
+
 	
 	std::lock_guard<std::mutex> lg(mtx);
 	
@@ -88,11 +97,6 @@ void get_work(std::stack<std::pair<typename T::iterator, typename T::iterator>>&
 		auto current_task = tasks.top();
 		tasks.pop();
 
-		//if (!tasks.empty()) {
-		//	cond.notify_all();
-		//}
-
-
 		busy_threads.push_back(std::this_thread::get_id());
 
 		ul.unlock();
@@ -112,9 +116,7 @@ void get_work(std::stack<std::pair<typename T::iterator, typename T::iterator>>&
 
 
 template<class T>
-void my_sort(typename T::iterator first, typename T::iterator last) {
-
-	size_t num_threads = std::thread::hardware_concurrency();
+void my_sort(typename T::iterator first, typename T::iterator last, size_t num_threads = NUM_THREADS) {
 	
 	std::vector<std::thread> threads;
 	std::vector<std::thread::id> busy_threads;
@@ -136,9 +138,12 @@ void my_sort(typename T::iterator first, typename T::iterator last) {
 int main(){
 	std::srand(0);
 
-	for (int i = 0; i < 100; i++) {
+	int start = std::clock();
 
-		std::vector<int> v(rand() % 1000);
+
+	for (int i = 0; i < 1; i++) {
+
+		std::vector<int> v(10000000);
 
 		for (int i = 0; i < v.size(); i++) {
 			v[i] = rand();
@@ -146,14 +151,19 @@ int main(){
 
 
 
-		auto v2 = v;
+		//auto v2 = v;
 
 		my_sort<std::vector<int>>(v.begin(), v.end());
 
-		std::sort(v2.begin(), v2.end());
+		//std::sort(v2.begin(), v2.end());
 
-		bool b = v2 == v;
+		//bool b = v2 == v;
 
-		std::cout << (int)b << " size: " << v.size() << std::endl;
+		//std::cout << (int)b << " size: " << v.size() << std::endl;
 	}
+
+
+	int end = std::clock();
+
+	std::cout << (double)(end - start) / CLOCKS_PER_SEC << " seconds";
 }
